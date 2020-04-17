2c926708101a92792d17f71a2b3493974abfc6b3
Stefano <stefano2890@gmail.com>
2020-04-17 15:54:40 +0200

    done

---
+++ main.js
@@ -0,0 +1,12 @@
+//chiedere il nome
+var nome = prompt('Qual\' è il tuo nome');
+//chiedere il cognome
+var cognome = prompt('Qual\'è il tuo cognome');
+// chiedere il colore preferito
+var colore = prompt('Qual\'è il tuo colore preferito');
+// numero casuale da aggiungere all fine della password
+var numero = 20;
+// prova in console
+console.log (nome + cognome + colore + numero)
+// stampa diretta in pagina
+document.writeln(nome + cognome + colore + numero)
